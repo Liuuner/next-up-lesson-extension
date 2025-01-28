6601821123adb7567a7d40b27fc05d0321009852
liuuner <liun@tappolet.ch>
2025-01-28 16:04:03 +0100

    v1 works

--- nextUpLessonIndicator.ts
+++ nextUpLessonIndicator.ts
@@ -28,17 +28,16 @@ export class LessonStatusIndicator extends PanelMenu.Button {
     _timer: number | undefined;
     _lessons: { [key: string]: Lesson[] } = {};
     // @ts-ignore
-    // _notifyItem: PopupMenu.PopupMenuItem;
-    // _reloadLessonsItem: PopupMenu.PopupMenuItem;
+    _notifyItem: PopupMenu.PopupMenuItem;
+    _reloadLessonsItem: PopupMenu.PopupMenuItem;
     _settings: Gio.Settings;
-    // menu: PopupMenu.PopupMenu;
 
     static {
         GObject.registerClass(this);
     }
 
     constructor(settings: Gio.Settings) {
-        super(0.0, "LessonStatusIndicator", false);
+        super(0.0, "LessonStatusIndicator");
 
         this._settings = settings;
 
@@ -49,64 +48,23 @@ export class LessonStatusIndicator extends PanelMenu.Button {
         });
         this.add_child(this.label);
 
-        // this.connect('button-press-event', () => this.menu.toggle());
-
         this._loadLessons().then(r => this._updateLesson());
-        // this._timer = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, this._settings.get_int("update-interval"), this._updateLesson);
-
-
-        // this.menu.toggle();
-        // Add action to toggle if the notification should be shown
-        // this._notifyItem = new PopupMenu.PopupMenuItem("Notify");
-
-        // this._notifyItem.connect('activate', () => this._toggleSettings('notify'));
-
-        if (this.menu) {
-            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem(_('Show')));
-        }
-
-        // this.menu.addMenuItem(
-        //     new PopupMenu.PopupSeparatorMenuItem(_('Show')));
-
-        /*this._cpuItem = this.menu.addAction(_('CPU'),
-            () => this._toggleSettings('show-cpu'));
-        this._memItem = this.menu.addAction(_('Memory'),
-            () => this._toggleSettings('show-memory'));
-        this._swapItem = this.menu.addAction(_('Swap'),
-            () => this._toggleSettings('show-swap'));
-        this._ulItem = this.menu.addAction(_('Upload'),
-            () => this._toggleSettings('show-upload'));
-        this._dlItem = this.menu.addAction(_('Download'),
-            () => this._toggleSettings('show-download'));*/
-
-        // this._notifyItem = this.menu.addAction(_('Notify'),
-        //     () => this._notify("Hello"));
-            // () => this._toggleSettings('notify'));
-
-        // this._notifyItem = this.
-
-
-        // this._appMenuItem = this.menu.addAction(_('Open System Monitor'),
-        //     () => this._openSystemMonitor());
-
-
-        // this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        // Main.panel.menuManager.addMenu(this.menu);
-
-        //---------------------------------------------------------------
-        // @ts-ignore
-        // this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        // @ts-ignore
-        // this._notifyItem = this.menu.addAction(_('Notify'),
-        //     () => this._toggleSettings('notify'));
 
         // @ts-ignore
-        // this._reloadLessonsItem = this.menu.addAction(_('Reload lessons'),
-        //     () => this._loadLessons().then(r => this._updateLesson()));
+        this._notifyItem = this.menu.addAction(_('Notify'),
+            () => this._toggleSettings('notify'));
+
+        // @ts-ignore
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem("Actions"));
+
+        // @ts-ignore
+        this._reloadLessonsItem = this.menu.addAction(_('Reload lessons'),
+            () => this._loadLessons().then(r => this._updateLesson()));
 
 
         this._settings.connect('changed', () => this._sync());
 
+        // @ts-ignore
         // this._settings.connectObject('changed',
         //     () => this._sync(), this);
         this._sync();
@@ -134,22 +92,17 @@ export class LessonStatusIndicator extends PanelMenu.Button {
 
 
     _sync() {
-        // this._notifyItem.setOrnament(this._settings.get_boolean('notify')
-        //     ? PopupMenu.Ornament.CHECK
-        //     : PopupMenu.Ornament.NONE);
+        this._notifyItem.setOrnament(this._settings.get_boolean('notify')
+            ? PopupMenu.Ornament.CHECK
+            : PopupMenu.Ornament.NONE);
         this._clearTimeout();
         this._ensureTimeout();
     }
 
     async _loadLessons() {
-        // const filePath = GLib.build_filenamev([GLib.get_home_dir(), ".local","share","gnome-shell","extensions/next-up-lesson-indicator@josephuspaye"]);
-        // const filePath = GLib.get_home_dir() + "/.local/share/gnome-shell/extensions/next-up-lesson-indicator@liuuner.ch/lessons.js";
-        // const filePath = this._settings.get_string('lessons-file');
-        // const filePath = "./lessons.js";
         try {
             const module = await import("./lessons.js");
             this._lessons = module.default; // Access the default export if applicable
-            // this._lessons = Lessons;
         } catch (error) {
             Main.notify("Failed to load lessons");
             throw error;
@@ -168,7 +121,6 @@ export class LessonStatusIndicator extends PanelMenu.Button {
 
     getCurrentLesson() {
         let now = new Date();
-        // @ts-ignore
         let dayNum = GLib.DateTime.new_now_local().get_day_of_week();
         let day = getDayName(dayNum);
         let currentTime = now.toLocaleTimeString('en-US', {
@@ -206,7 +158,7 @@ export class LessonStatusIndicator extends PanelMenu.Button {
 
     destroy() {
         if (this._timer) GLib.source_remove(this._timer);
-        // if (this._notifyItem) this._notifyItem.destroy();
+        if (this._notifyItem) this._notifyItem.destroy();
         if (this.label) this.label.destroy();
         if (this._timer) this._clearTimeout();
         super.destroy();
