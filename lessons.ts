501bf5edcad9f61996c00905729970b4f5023700
liuuner <liun@tappolet.ch>
2025-08-18 09:04:01 +0200

    update lessons

--- lessons.ts
+++ lessons.ts
@@ -1,30 +1,24 @@
 export default {
     "monday": [
-        {start: "08:25", end: "09:10", name: "Math"},
-        {start: "09:15", end: "10:00", name: "Math"},
-
-        {start: "10:20", end: "11:05", name: "Math"},
-        {start: "11:10", end: "11:55", name: "W&R"},
-
-        {start: "12:55", end: "13:40", name: "Physics"},
-        {start: "13:45", end: "14:30", name: "Physics"},
-        {start: "14:35", end: "15:20", name: "G&P"},
-
-        {start: "15:35", end: "16:20", name: "Deutsch"},
-        {start: "16:25", end: "17:10", name: "Deutsch"},
+        { start: "07:35", end: "08:20", name: "Innovative Projekte" },
+        { start: "08:25", end: "09:10", name: "Innovative Projekte" },
+        { start: "09:15", end: "10:00", name: "Verteilte Systeme" },
+        { start: "10:20", end: "11:05", name: "Verteilte Systeme" },
+        { start: "11:10", end: "11:55", name: "Sport" },
+        { start: "13:45", end: "14:30", name: "DevOps" },
+        { start: "14:35", end: "15:20", name: "DevOps" },
+        { start: "15:35", end: "16:20", name: "Testen" },
+        { start: "16:25", end: "17:10", name: "Testen" },
     ],
     "tuesday": [
-        {start: "07:55", end: "08:40", name: "Sport"},
-        {start: "09:15", end: "10:00", name: "Vogel"},
-
-        {start: "10:20", end: "11:05", name: "Vogel"},
-        {start: "11:10", end: "11:55", name: "Kleinprojekte"},
-        {start: "12:00", end: "12:45", name: "Kleinprojekte"},
-
-        {start: "13:45", end: "14:30", name: "FiMa"},
-        {start: "14:35", end: "15:20", name: "FiMa"},
-
-        {start: "15:35", end: "16:20", name: "Functional"},
-        {start: "16:25", end: "17:10", name: "Functional"},
+        { start: "07:35", end: "08:20", name: "BMA" },
+        { start: "08:25", end: "09:10", name: "Math" },
+        { start: "09:15", end: "10:00", name: "Math" },
+        { start: "10:20", end: "11:05", name: "G&P" },
+        { start: "11:10", end: "11:55", name: "G&P" },
+        { start: "12:55", end: "13:40", name: "Deutsch" },
+        { start: "13:45", end: "14:30", name: "Deutsch" },
+        { start: "14:35", end: "15:20", name: "Physics" },
+        { start: "15:35", end: "16:20", name: "Physics" },
     ],
-}
+};
